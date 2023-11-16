9b216d3ef4b892425000d9afe60bd70f744f6e20
Hiroto Shioi <shioihigg@gmail.com>
2023-11-16 20:55:29 +0900

    フォーム

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>DALL-E 2</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent class="ion-padding" fullscreen>
+      <div class="media-container">
+        <div class="token-input">
+          <IonInput
+            fill="outline"
+            mode="md"
+            placeholder="Tokenを入力してください"
+            v-model="token"
+          />
+        </div>
+        <div class="prompt-input">
+          <IonInput
+            fill="outline"
+            mode="md"
+            placeholder="画像の説明を入力してください"
+            v-model="prompt"
+          />
+        </div>
+      </div>
+    </IonContent>
+  </IonPage>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { ref } from "vue";
+
+const token = ref("");
+const prompt = ref("");
+</script>
+
+<style scoped lang="scss">
+.token-input {
+  margin-bottom: 10px;
 }
 </style>
