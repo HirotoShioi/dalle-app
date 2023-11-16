96461e56f5936cb61d8b4e5c36f49c7a124d6b52
Hiroto Shioi <shioihigg@gmail.com>
2023-11-16 21:59:06 +0900

    イメージ表示

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,63 @@
   <IonPage>
     <IonHeader>
       <IonToolbar>
-        <IonTitle>DALL-E 2</IonTitle>
+        <IonTitle>DALL-E 3</IonTitle>
       </IonToolbar>
+      <IonProgressBar
+        v-if="isSubmitting"
+        type="indeterminate"
+        color="primary"
+      />
     </IonHeader>
     <IonContent class="ion-padding" fullscreen>
       <div class="media-container">
-        <div class="token-input">
-          <IonInput
-            fill="outline"
-            mode="md"
-            placeholder="Tokenを入力してください"
-            v-model="token"
-          />
-        </div>
-        <div class="prompt-input">
-          <IonInput
-            fill="outline"
-            mode="md"
-            placeholder="画像の説明を入力してください"
-            v-model="prompt"
-          />
-        </div>
+        <form @submit.prevent="submit">
+          <div class="form-group">
+            <IonInput
+              fill="outline"
+              mode="md"
+              placeholder="Tokenを入力してください"
+              v-model="token"
+            />
+          </div>
+          <div class="form-group">
+            <IonInput
+              fill="outline"
+              mode="md"
+              placeholder="画像の説明を入力してください"
+              v-model="prompt"
+            />
+          </div>
+          <IonButton
+            type="submit"
+            expand="block"
+            color="primary"
+            :disabled="isSubmitting"
+            >送信</IonButton
+          >
+        </form>
+      </div>
+      <div class="generated-images">
+        <IonGrid>
+          <IonRow>
+            <IonCol
+              size="12"
+              size-sm="12"
+              size-md="3"
+              size-lg="3"
+              size-xl="3"
+              v-for="(image, i) in images"
+              :key="image.url"
+            >
+              <IonCard>
+                <div class="image-container">
+                  <img :src="image.url" />
+                </div>
+                <IonCardContent> #{{ i + 1 }} </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </div>
     </IonContent>
   </IonPage>
@@ -30,21 +66,78 @@
 
 <script setup lang="ts">
 import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
   IonInput,
   IonPage,
+  IonProgressBar,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from "@ionic/vue";
 import { ref } from "vue";
+import OpenAI from "openai";
 
 const token = ref("");
 const prompt = ref("");
+const isSubmitting = ref(false);
+const images = ref<OpenAI.Image[]>([]);
+const submit = async () => {
+  if (!token.value) {
+    alert("Tokenを入力してください");
+    return;
+  }
+  if (!prompt.value) {
+    alert("画像の説明を入力してください");
+    return;
+  }
+  isSubmitting.value = true;
+  // 現在のレートリミットを調べる
+  const result = await fetch("https://api.openai.com/v1/images/generations", {
+    headers: {
+      Authorization: `Bearer ${token.value}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      prompt: prompt.value,
+      model: "dall-e-3",
+      n: 1,
+      quality: "standard",
+      response_format: "url",
+      size: "1024x1024",
+    }),
+  }).finally(() => {
+    isSubmitting.value = false;
+  });
+  console.log(result);
+  result.headers.forEach((value, key) => {
+    console.log({
+      key,
+      value,
+    });
+  });
+  const fetchedImage = await result.json();
+  console.log(fetchedImage);
+  images.value = [...images.value, ...(fetchedImage.data as OpenAI.Image[])];
+};
 </script>
 
 <style scoped lang="scss">
-.token-input {
+.form-group {
   margin-bottom: 10px;
+}
+.generated-images {
+  ion-card {
+    margin-bottom: 10px;
+    img {
+      width: 100%;
+    }
+  }
 }
 </style>
